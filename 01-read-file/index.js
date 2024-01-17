e0df7edc91c202797eb9dea2f65d837ce81c8135
Tanya Holikava <holikavadev@gmail.com>
2024-01-17 19:57:20 +0300

    feat: done 01-read-file task

---
+++ 01-read-file/index.js
@@ -0,0 +1,12 @@
+const fs = require('fs');
+const path = require('path');
+
+const textPath = path.resolve(__dirname, 'text.txt');
+
+const output = fs.createReadStream(textPath, 'utf-8');
+
+output.on('data', (data) => {
+  console.log('Content is: ' + data.toString());
+});
+
+console.log('continue...');
