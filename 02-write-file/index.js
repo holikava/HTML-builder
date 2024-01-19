e2b424e3ca1a3f9a1f4e293fe93b352ed1f0b8e8
Tanya Holikava <holikavadev@gmail.com>
2024-01-19 22:35:30 +0300

    feat: add exit to task 02

--- 02-write-file/index.js
+++ 02-write-file/index.js
@@ -9,10 +9,16 @@ fs.open(textFilePath, 'w', (err) => {
   if (err) throw err;
 });
 
-stdout.write('What about you thinking?\n');
+stdout.write('What do you like?\n');
 stdin.on('data', (data) => {
+  if (data.toString().trim().toLowerCase() === 'exit') {
+    process.exit();
+  }
   fs.appendFile(textFilePath, data, (err) => {
     if (err) throw err;
     console.log('added! next >');
   });
 });
+
+process.on('SIGINT', process.exit);
+process.on('exit', () => stdout.write('Done. Have a nice day!'));
